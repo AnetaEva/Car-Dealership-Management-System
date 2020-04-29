3c531c15d7de58532fc93445f74da69078f5a545
AnetaEva <60235389+AnetaEva@users.noreply.github.com>
2020-04-29 14:28:26 -0400

    First Commit

---
+++ src/edu/psu/abington/ist/ist242/Main.java
@@ -0,0 +1,10 @@
+package edu.psu.abington.ist.ist242;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+	// write your code here
+    }
+}
